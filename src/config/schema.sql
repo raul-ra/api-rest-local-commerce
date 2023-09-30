a41ddaab6dadea721f277bd3e50135bd1eb3bcaa
raul-ra <raulradilloarevalo@gmail.com>
2023-09-30 13:52:43 +0200

    create Schema.sql

---
+++ src/config/schema.sql
@@ -0,0 +1,19 @@
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS local_commerce;
+USE local_commerce;
+
+-- Create the table of brands
+CREATE TABLE IF NOT EXISTS brands (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Create the table of products
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    brand_id INT,
+    FOREIGN KEY (brand_id) REFERENCES brands (id)
+);
