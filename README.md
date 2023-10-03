f0292fc7b3a46a52d8c9c17f7622ef94737099f2
raul-ra <raulradilloarevalo@gmail.com>
2023-10-03 14:11:01 +0200

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,197 @@
-# api-rest-local-commerce
+# Local Commerce REST API
+
+This project is a REST API for a local commerce developed with Node.js, leveraging Sequelize as an ORM, and Jest alongside Supertest for testing.
+
+## Technologies Used
+
+- Node.js
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+
+- Sequelize (ORM)
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg" />
+
+- MySQL (Database)
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" />
+
+- Jest (Unit Testing)
+
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
+
+
+- Supertest (Endpoint Testing)
+
+## Pre-requisites
+
+- Node.js
+- MySQL
+
+## Initial Setup
+
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/raul-ra/api-rest-local-commerce.git
+   cd api-rest-local-commerce
+   ```
+
+2. **Install dependencies**:
+   ```bash
+   npm install
+   ```
+
+3. **Install Sequelize, Jest, Supertest, and MySQL**:
+   - Sequelize: `npm install sequelize sequelize-cli`
+   - Jest: `npm install jest`
+   - Supertest: `npm install supertest`
+   - MySQL: `npm install mysql2`
+
+4. **Database Setup**:
+   - Ensure MySQL is running.
+   - Import the databases from `schema.sql` for the development environment and `schema_test.sql` for testing:
+     ```bash
+     mysql -u [YOUR_MYSQL_USERNAME] -p < schema.sql
+     mysql -u [YOUR_MYSQL_USERNAME] -p < schema_test.sql
+     ```
+
+**Flow Diagram**:
+
+https://dbdiagram.io/d/api-rest-local-commerce-6511626fffbf5169f06f89f9
+
+5. **.env File Setup**:
+   - Create a `.env` file in the project root.
+   - Add the following environment variables:
+     ```
+     DB_USERNAME=[YOUR_MYSQL_USERNAME]
+     DB_PASSWORD=[YOUR_MYSQL_PASSWORD]
+     DB_NAME=local_commerce
+     DB_HOST=127.0.0.1
+     DB_DIALECT=mysql
+     ```
+
+6. **.gitignore File**:
+   Ensure `.gitignore` contains at least the following entries to prevent uploading sensitive or unnecessary files to the repository:
+   ```
+   node_modules/
+   .env
+   ```
+
+## Execution
+
+To start the API in development mode:
+```bash
+npm start
+```
+
+The API should be running at `http://localhost:3000`.
+
+## Testing
+
+To run unit tests:
+```bash
+npm test
+```
+
+## Testing with Postman
+
+1. **Install Postman**: If you don't have Postman yet, you can download it from [here](https://www.postman.com/downloads/).
+
+2. **Endpoints**:
+   - **Brands**:
+     - Get all brands: GET `http://localhost:3000/brands`
+     - Get a brand by ID: GET `http://localhost:3000/brands/:id`
+     - Create a new brand: POST `http://localhost:3000/brands`
+     - Update a brand: PUT `http://localhost:3000/brands/:id`
+     - Delete a brand: DELETE `http://localhost:3000/brands/:id`
+   
+   - **Products**:
+     - Get all products: GET `http://localhost:3000/products`
+     - Get a product by ID: GET `http://localhost:3000/products/:id`
+     - Create a new product: POST `http://localhost:3000/products`
+     - Update a product: PUT `http://localhost:3000/products/:id`
+     - Delete a product: DELETE `http://localhost:3000/products/:id`
+
+3. Postman Usage Examples
+
+### Brands
+
+1. **Get all brands**:
+   - Method: `GET`
+   - URL: `http://localhost:3000/brands`
+
+2. **Get a brand by ID**:
+   - Method: `GET`
+   - URL: `http://localhost:3000/brands/1`
+   - Note: Replace `1` with the ID of the brand you wish to retrieve.
+
+3. **Create a new brand**:
+   - Method: `POST`
+   - URL: `http://localhost:3000/brands`
+   - Body:
+     ```json
+     {
+       "name": "Sample Brand"
+     }
+     ```
+
+4. **Update a brand**:
+   - Method: `PUT`
+   - URL: `http://localhost:3000/brands/1`
+   - Body:
+     ```json
+     {
+       "name": "Updated Brand"
+     }
+     ```
+   - Note: Replace `1` with the ID of the brand you wish to update.
+
+5. **Delete a brand**:
+   - Method: `DELETE`
+   - URL: `http://localhost:3000/brands/1`
+   - Note: Replace `1` with the ID of the brand you wish to delete.
+
+### Products
+
+1. **Get all products**:
+   - Method: `GET`
+   - URL: `http://localhost:3000/products`
+
+2. **Get a product by ID**:
+   - Method: `GET`
+   - URL: `http://localhost:3000/products/1`
+   - Note: Replace `1` with the ID of the product you wish to retrieve.
+
+3. **Create a new product**:
+   - Method: `POST`
+   - URL: `http://localhost:3000/products`
+   - Body:
+     ```json
+     {
+       "name": "Sample Product",
+       "price": 50.0,
+       "stock": 20,
+       "brand_id": 1
+     }
+     ```
+   - Note: Ensure the `brand_id` corresponds to an existing brand.
+
+4. **Update a product**:
+   - Method: `PUT`
+   - URL: `http://localhost:3000/products/1`
+   - Body:
+     ```json
+     {
+       "name": "Updated Product",
+       "price": 55.0
+     }
+     ```
+   - Note: Replace `1` with the ID of the product you wish to update.
+
+5. **Delete a product**:
+   - Method: `DELETE`
+   - URL: `http://localhost:3000/products/1`
+   - Note: Replace `1` with the ID of the product you wish to delete.
+
+I hope these instructions are helpful to you.
--- src/config/schema_test.sql
+++ src/config/schema_test.sql
@@ -1,16 +1,14 @@
 /*Important: to run unit testing go first to .env*/
 
--- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS local_commerce_fake;
 USE local_commerce_fake;
 
--- Create the table of brands
+
 CREATE TABLE IF NOT EXISTS brands (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
--- Create the table of products
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -19,3 +17,180 @@ CREATE TABLE IF NOT EXISTS products (
     brand_id INT,
     FOREIGN KEY (brand_id) REFERENCES brands (id)
 );
+
+USE local_commerce_fake;
+
+ALTER TABLE brands 
+MODIFY updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+INSERT INTO brands (name) VALUES 
+('4ECO'), 
+('A.VOGEL'), 
+('ABBOT KINNEY`S'), 
+('ACORELLE'), 
+('AGRECO'), 
+('AH TABLE'), 
+('ALB-GOLD'), 
+('ALCE NERO'), 
+('ALGA MARIS'), 
+('ALGAMAR'), 
+('ALIÑO'), 
+('ALLOS'), 
+('ALMENDRINA'), 
+('ALONSO DE LA TORRE'), 
+('ALTERNATIVA3'), 
+('AMAIZIN'), 
+('AMANDIN'), 
+('AMAPOLA BIOCOSMETICS');
+
+
+DESCRIBE products;
+
+ALTER TABLE products
+ADD FOREIGN KEY (brand_id) REFERENCES brands(id);
+
+
+ALTER TABLE products 
+MODIFY createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE products 
+MODIFY updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Spaghetti 4ECO', 10.00, 20, 1), 
+('Arroz 4ECO', 12.00, 25, 1), 
+('Lentejas 4ECO', 8.00, 30, 1), 
+('Garbanzos 4ECO', 9.00, 15, 1), 
+('Fideos 4ECO', 11.00, 10, 1);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Muesli A.VOGEL', 15.00, 30, 2), 
+('Jugo Aloe Vera A.VOGEL', 20.00, 20, 2), 
+('Echinacea A.VOGEL', 25.00, 15, 2), 
+('Aceite de Coco A.VOGEL', 22.00, 10, 2), 
+('Té Verde A.VOGEL', 18.00, 25, 2);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Yogurt ABBOT KINNEY`S', 20.00, 10, 3), 
+('Leche de Almendra ABBOT KINNEY`S', 15.00, 20, 3), 
+('Smoothie ABBOT KINNEY`S', 12.00, 25, 3), 
+('Helado ABBOT KINNEY`S', 10.00, 30, 3), 
+('Tofu ABBOT KINNEY`S', 18.00, 15, 3);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Perfume ACORELLE', 30.00, 10, 4), 
+('Desodorante ACORELLE', 20.00, 20, 4), 
+('Crema ACORELLE', 25.00, 15, 4), 
+('Aceite Esencial ACORELLE', 35.00, 5, 4), 
+('Loción ACORELLE', 28.00, 12, 4);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Aceite de Oliva AGRECO', 15.00, 20, 5), 
+('Vinagre AGRECO', 10.00, 30, 5), 
+('Tomate Frito AGRECO', 8.00, 25, 5), 
+('Miel AGRECO', 12.00, 15, 5), 
+('Mermelada AGRECO', 9.00, 20, 5);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Servilletas AH TABLE', 5.00, 50, 6), 
+('Vasos AH TABLE', 10.00, 30, 6), 
+('Platos AH TABLE', 15.00, 20, 6), 
+('Cubiertos AH TABLE', 12.00, 25, 6), 
+('Mantel AH TABLE', 20.00, 10, 6);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Macarrones ALB-GOLD', 10.00, 20, 7), 
+('Espaguetis ALB-GOLD', 11.00, 25, 7), 
+('Fusilli ALB-GOLD', 12.00, 30, 7), 
+('Penne ALB-GOLD', 13.00, 15, 7), 
+('Farfalle ALB-GOLD', 14.00, 10, 7);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Miel ALCE NERO', 20.00, 15, 8), 
+('Pasta ALCE NERO', 15.00, 20, 8), 
+('Arroz ALCE NERO', 18.00, 25, 8), 
+('Galletas ALCE NERO', 10.00, 30, 8), 
+('Jugo ALCE NERO', 12.00, 20, 8);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Protector Solar ALGA MARIS', 25.00, 20, 9), 
+('After Sun ALGA MARIS', 20.00, 30, 9), 
+('Champú ALGA MARIS', 15.00, 25, 9), 
+('Gel de Ducha ALGA MARIS', 12.00, 35, 9), 
+('Loción Corporal ALGA MARIS', 18.00, 15, 9);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Algas Wakame ALGAMAR', 10.00, 20, 10), 
+('Algas Nori ALGAMAR', 12.00, 25, 10), 
+('Algas Kombu ALGAMAR', 15.00, 30, 10), 
+('Algas Dulse ALGAMAR', 14.00, 15, 10), 
+('Algas Espagueti de Mar ALGAMAR', 16.00, 10, 10);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Vinagre de Manzana ALIÑO', 5.00, 30, 11), 
+('Vinagre de Vino ALIÑO', 6.00, 25, 11), 
+('Aceite de Oliva Virgen Extra ALIÑO', 10.00, 20, 11), 
+('Sal Marina ALIÑO', 3.00, 40, 11), 
+('Pimienta Negra ALIÑO', 4.00, 20, 11);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Barrita de Cereal ALLOS', 2.00, 50, 12), 
+('Mermelada de Fresa ALLOS', 4.00, 30, 12), 
+('Miel de Flores ALLOS', 7.00, 20, 12), 
+('Zumo de Manzana ALLOS', 3.00, 40, 12), 
+('Galletas de Avena ALLOS', 5.00, 25, 12);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Leche de Almendra ALMENDRINA', 3.00, 30, 13), 
+('Crema de Almendra ALMENDRINA', 5.00, 20, 13), 
+('Harina de Almendra ALMENDRINA', 6.00, 15, 13), 
+('Almendra Tostada ALMENDRINA', 8.00, 20, 13), 
+('Almendra Cruda ALMENDRINA', 7.00, 25, 13);
+
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Jamón Ibérico ALONSO DE LA TORRE', 50.00, 10, 14), 
+('Queso Curado ALONSO DE LA TORRE', 20.00, 20, 14), 
+('Chorizo Ibérico ALONSO DE LA TORRE', 15.00, 25, 14), 
+('Lomo Ibérico ALONSO DE LA TORRE', 30.00, 15, 14), 
+('Salchichón Ibérico ALONSO DE LA TORRE', 12.00, 30, 14);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Café ALTERNATIVA3', 10.00, 20, 15), 
+('Chocolate ALTERNATIVA3', 5.00, 30, 15), 
+('Azúcar ALTERNATIVA3', 3.00, 40, 15), 
+('Té ALTERNATIVA3', 7.00, 25, 15), 
+('Cacao ALTERNATIVA3', 8.00, 15, 15);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Maíz Dulce AMAIZIN', 4.00, 20, 16), 
+('Leche de Coco AMAIZIN', 6.00, 25, 16), 
+('Sopa de Tomate AMAIZIN', 5.00, 30, 16), 
+('Tortillas AMAIZIN', 8.00, 15, 16), 
+('Salsa de Tomate AMAIZIN', 3.00, 20, 16);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Bebida de Avena AMANDIN', 3.00, 20, 17), 
+('Crema de Cacao AMANDIN', 5.00, 25, 17), 
+('Caldo Vegetal AMANDIN', 2.00, 30, 17), 
+('Sopa de Miso AMANDIN', 4.00, 15, 17), 
+('Turrón AMANDIN', 6.00, 20, 17);
+
+INSERT INTO products (name, price, stock, brand_id) VALUES 
+('Crema Hidratante AMAPOLA BIOCOSMETICS', 20.00, 20, 18), 
+('Aceite Esencial AMAPOLA BIOCOSMETICS', 15.00, 25, 18), 
+('Jabón Natural AMAPOLA BIOCOSMETICS', 10.00, 30, 18), 
+('Champú Sólido AMAPOLA BIOCOSMETICS', 12.00, 15, 18), 
+('Mascarilla Facial AMAPOLA BIOCOSMETICS', 18.00, 20, 18);
+
+
+
+
